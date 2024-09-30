baa8bad8ea7141687d0d0b844c06adae496f2152
SpeedieTest <speediestester@gmail.com>
2024-09-30 22:43:07 +1000

    Update to Index page

--- Python Project/app/static/js/taskbar.js
+++ Python Project/app/static/js/taskbar.js
@@ -3,7 +3,7 @@ function updateTime() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const timeString = `${hours}:${minutes}`;
     document.getElementById('time').innerText = timeString;
 }
 
